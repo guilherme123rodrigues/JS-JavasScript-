1f7d01f2cb73622cf6d070b5b0dd6761af33f793
Guilherme Rodriguese de Souza <guigithub001@gmail.com>
2026-09-07 13:41:01 -0300

    Update ex008.js
    Condição "IF"

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,1 +1,12 @@
-console.log("Olá, Mundo!")
+console.log('Olá,Mundo!')
+
+hoje = ''
+
+if (hoje === true) {
+    console.log('Iniciando estudos em JS, em condições (if, else if e else)')
+} else if (hoje === false) {
+    console.log('Hojê não é um dia bom para estudo.')
+}
+else {
+    console.log('Algo deu errado :(')
+}
