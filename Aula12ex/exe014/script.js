ff49c11be26ae9e97ee1fc9ae27ebc8a56920b4d
Guilherme Rodriguese de Souza <guigithub001@gmail.com>
2026-09-26 00:13:00 -0300

    Criando o javascript

--- Aula12ex/exe014/script.js
+++ Aula12ex/exe014/script.js
@@ -1,5 +1,12 @@
 function verificar() {
+    let ano_nascimetno = window.document.querySelector("#ano_nascimento")
     let mensagem = window.document.querySelector("#msg")
+    let sexo = window.document.querySelector("#sexo")
+    let data = new Date()
+    let ano = data.getFullYear()
+    let idade = ano - Number(ano_nascimetno)
 
-    mensagem.textContent = 'Um Homem que nasceu em 2000 tem 25 anos'
+    if (Number(ano_nascimetno.value) < 1970) {
+        mensagem.textContent = `Um ${sexo[0].value} que nasceu em ${ano_nascimetno} tem ${idade} anos`
+    }
 }
